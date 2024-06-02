b69abd263a0f4fbc0fbcc7533fc12cc138d38f19
devang64 <pateldevang6432@gmail.com>
2024-06-02 23:25:07 +0530

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { getServerSession } from "next-auth";
+import SessionProvider from "@/utils/SessionProvider";
+import Navbar from "@/components/Navbar";
+import ToastProviders from "@/ToastProviders";
+import img from '../../public/logo 1.png'
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Next Auth BoiletPlate",
+  description: "created by devang patel",
+  icons: [{ rel: "icon", url: "./icon.ico" }]
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const session = await getServerSession();
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <SessionProvider session={session}>
+          <div className="mx-auto max-w-5xl text-2xl gap-2 mb-10">
+            <Navbar />
+            <ToastProviders>
+              {children}
+            </ToastProviders>
+          </div>
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+import React, { useEffect } from "react";
+import { useSession } from 'next-auth/react'
+import Image from "next/image";
+import toast from "react-hot-toast";
+
+const Profile = () => {
+  const { data: session, status } = useSession()
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      toast.error("Access denied!")
+    }
+  }, [status])
+  if (status === 'loading') {
+    return (
+      <>
+        <div className="flex flex-col p-24 items-center gap-8">
+          <p>Loading...</p>
+        </div>
+      </>
+    )
+  }
+
+  if (status === 'unauthenticated') {
+    return (<>
+      <div className="flex flex-col p-24 items-center gap-8">
+        <p>Access Denied</p>
+      </div>
+    </>)
+  }
+
+  return (
+    <div className="flex flex-col p-24 items-center gap-8">
+      <div className="flex items-center gap-4">
+        <Image src={session?.user?.image!} alt="" width={60} height={60} className="rounded-full" />
+        <h1>Welcome, {session?.user?.name}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
