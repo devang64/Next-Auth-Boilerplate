8d8a32b04d2a774c57fce35f97e4e5f24da1adab
devang64 <pateldevang6432@gmail.com>
2024-06-02 23:28:17 +0530

    Fixed: logo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -64,7 +64,7 @@ const NextLoginPage = () => {
     sessionStatus !== "authenticated" && (
       <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="flex justify-center flex-col items-center">
-          <Image src="/logo 1.png" alt="star logo" width={50} height={50} />
+          <Image src="/logo.png" alt="" width={50} height={50} />
           <h2 className="mt-6 text-center text-2xl leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
